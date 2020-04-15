8d55bd9bc4bd36c6df629ecef8ad39c910a90e90
Marshall Williams <marshall@mwthink.net>
2020-04-14 19:54:44 -0500

    finally rdy to go

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,29 @@
+import * as Http from 'http';
+import * as Path from 'path';
+import * as Express from 'express';
 import * as SocketIO from 'socket.io'
 import { ChatServerI } from './interfaces';
 import { SocketChatServer } from './entities';
 
-const io = SocketIO(3000);
+const port = 3000;
+const app = Express();
+const server = Http.createServer(app);
+const io = SocketIO(server);
 
 const chatServer: ChatServerI = new SocketChatServer(io);
+
+if( (process.env['SERVE_STATIC']||'false').toLowerCase() === 'true' ){
+  const staticPath = Path.resolve(__dirname, '../client');
+  console.log('Serving static assets from', staticPath);
+  app.use(Express.static(staticPath));
+}
+
+Promise.resolve()
+.then(() => (
+  server.listen(port, () => {
+    console.log('Listening on port', port);
+  })
+))
 
 function shutdown(){
   chatServer.send('Server is shutting down');
